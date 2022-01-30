727ea795a5e35f84da60086205a62f5411dd40ef
nelsonagurtop@gmail.com <nelsonagurtop@gmail.com>
2022-01-29 21:28:26 -0400

    Cambio a eleccion de stage

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -30,7 +30,7 @@ def call(){
 										
 	                if (params.buildTool == "gradle") {
 						figlet 'Pipeline   Gradle'
-						//echo "escribio stage, ${stage}"
+						echo "escribio stage, ${stage}"
 	                    gradle()
 	                } else {
 						figlet 'Pipeline   Maven'
