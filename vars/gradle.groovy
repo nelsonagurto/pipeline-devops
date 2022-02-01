edba44292cea59a93aa5b9c0995d2a18535e74ea
nelsonagurtop@gmail.com <nelsonagurtop@gmail.com>
2022-01-31 21:40:05 -0400

    actualizo arch. groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -1,87 +1,34 @@
-void call(String[] stagesToRun) {
-    String stageBuild = 'build'
-    String stageSonar = 'sonar'
-    String stageRun = 'run'
-    String stageTestRun = 'test run'
-    String stageNexus = 'nexus'
-
-    String[] stages = [
-        stageBuild,
-        stageSonar,
-        stageRun,
-        stageTestRun,
-        stageNexus
-    ]
-
-    String[] currentStages = []
-
-    if (stagesToRun.size() == 1 && stagesToRun[0] == '') {
-        currentStages = stages
-    } else {
-        currentStages = stagesToRun
-    }
-
-    if (stages.findAll { e -> currentStages.contains( e ) }.size() == 0) {
-        throw new Exception('Al menos una stage es invalida. Stages validas: ' + stages.join(', ') + '. Recibe: ' + currentStages.join(', '))
-    }
-
-    if (currentStages.contains(stageBuild)) {
-        stage(stageBuild) {
-            CURRENT_STAGE = stageBuild
-            bat "./gradlew.cmd clean build"
-        }
-    }
-/*
-    if (currentStages.contains(stageSonar)) {
-        stage(stageSonar) {
-            CURRENT_STAGE = stageSonar
-            String scannerHome = tool 'sonar-scanner'
-            withSonarQubeEnv('sonarqube-server') {
-                bt "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build"
-            }
-        }
-    }
-
-    if (currentStages.contains(stageRun)) {
-        stage(stageRun) {
-            CURRENT_STAGE = stageRun
-            bat "./gradlew.cmd bootRun &"
-            sleep 20
-        }
-    }
-
+def call(){
+  	stage('build'){
+		figlet CURRENT_STAGE
+		//STAGE = env.STAGE_NAME
+		//println "Stage: ${env.STAGE_NAME}"
+		//echo "stage escrito: ${params.STAGE}"
+		//bat "./gradlew.bat clean build"  
+	}
 	
-    if (currentStages.contains(stageTestRun)) {
-        stage(stageTestRun) {
-            CURRENT_STAGE = stageTestRun
-            sh 'curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing'
-        }
-    }
+	stage('sonar'){
+		figlet CURRENT_STAGE
+		//STAGE = env.STAGE_NAME
+		println "Stage: ${env.STAGE_NAME}"
+	}
 
-	
-	
-    if (currentStages.contains(stageNexus)) {
-        stage(stageNexus) {
-            CURRENT_STAGE = stageNexus
-            nexusPublisher nexusInstanceId: 'nexus3-docker',
-        nexusRepositoryId: 'ejemplo-gradle',
-        packages: [
-            [
-                $class: 'MavenPackage',
-                mavenAssetList: [
-                    [classifier: '', extension: '', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']
-                ],
-                mavenCoordinate: [
-                    artifactId: 'DevOpsUsach2020',
-                    groupId: 'com.devopsusach2020',
-                    packaging: 'jar',
-                    version: '0.0.1'
-                ]
-            ]
-        ]
-        }
-    }
-*/
+	stage('run'){
+		figlet CURRENT_STAGE
+		//STAGE = env.STAGE_NAME
+		println "Stage: ${env.STAGE_NAME}"
+	}
+
+	stage('test'){
+		figlet CURRENT_STAGE
+		//STAGE = env.STAGE_NAME
+		println "Stage: ${env.STAGE_NAME}"
+	}
+
+	stage('nexus'){
+		figlet CURRENT_STAGE
+		//STAGE = env.STAGE_NAME
+		println "Stage: ${env.STAGE_NAME}"
+	}
 }
-
-return this
+return this;
--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,89 +1,29 @@
-void call(String[] stagesToRun) {
-    String stageBuild = 'build'
-    String stageSonar = 'sonar'
-    String stageRun = 'run'
-    String stageTestRun = 'test run'
-    String stageNexus = 'nexus'
+def call(){
 
-    String[] stages = [
-        stageBuild,
-        stageSonar,
-        stageRun,
-        stageTestRun,
-        stageNexus
-    ]
+  stage('compile') {
+    //STAGE = env.STAGE_NAME
+	figlet CURRENT_STAGE
+    //bat "./mvnw.cmd clean compile -e"
+  }
 
-    String[] currentStages = []
+  stage('test') {
+    //STAGE = env.STAGE_NAME
+	figlet CURRENT_STAGE
+    //bat "./mvnw.cmd clean test -e"
+  }
 
-    if (stagesToRun.size() == 1 && stagesToRun[0] == '') {
-        currentStages = stages
-    } else {
-        currentStages = stagesToRun
-    }
+  stage('jar') {
+    //STAGE = env.STAGE_NAME
+	figlet CURRENT_STAGE
+    //bat "./mvnw.cmd clean package -e"
+  }
 
-    if (stages.findAll { e -> currentStages.contains( e ) }.size() == 0) {
-        throw new Exception('Al menos una stage es invalida. Stages validas: ' + stages.join(', ') + '. Recibe: ' + currentStages.join(', '))
-    }
-
-    if (currentStages.contains(stageBuild)) {
-        stage(stageBuild) {
-            CURRENT_STAGE = stageBuild
-				dir('F:/Diplomado DevOps/Modulo III/nelson_agurto/pipeline-devops'){
-					bat "./mvnw.bat clean compile -e"
-					bat "./mvnw.cmd clean test -e"
-					bat "./mvnw.cmd clean package -e"
-				}
-        }
-    }
-
-/*
-    if (currentStages.contains(stageSonar)) {
-        stage(stageSonar) {
-            CURRENT_STAGE = stageSonar
-            def scannerHome = tool 'sonar-scanner'
-            withSonarQubeEnv('docker-compose-sonarqube') {
-                sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-maven -Dsonar.sources=src -Dsonar.java.binaries=build"
-            }
-        }
-    }
-
-    if (currentStages.contains(stageRun)) {
-        stage(stageRun) {
-            CURRENT_STAGE = stageRun
-            sh './mvnw spring-boot:run &'
-            sleep 20
-        }
-    }
-
-    if (currentStages.contains(stageTestRun)) {
-        stage(stageTestRun) {
-            CURRENT_STAGE = stageTestRun
-            sh 'curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing'
-        }
-    }
-
-    if (currentStages.contains(stageNexus)) {
-        stage(stageNexus) {
-            CURRENT_STAGE = stageNexus
-            nexusPublisher nexusInstanceId: 'nexus3-docker',
-        nexusRepositoryId: 'ejemplo-maven',
-        packages: [
-            [
-                $class: 'MavenPackage',
-                mavenAssetList: [
-                    [classifier: '', extension: '', filePath: 'build/DevOpsUsach2020-0.0.1.jar']
-                ],
-                mavenCoordinate: [
-                    artifactId: 'DevOpsUsach2020',
-                    groupId: 'com.devopsusach2020',
-                    packaging: 'jar',
-                    version: '0.0.1'
-                ]
-            ]
-        ]
-        }
-    }
-*/
+  stage('run') {
+    //STAGE = env.STAGE_NAME
+	figlet CURRENT_STAGE
+		//bat "start /min mvnw.cmd spring-boot:run &"
+		//sleep 20
+  }  
 }
 
-return this
+return this;
